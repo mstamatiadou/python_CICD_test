4a3adc20e4c72c3d71bd81430cc097664f0f7217
mstamatiadou <marina.stamatiadou@gmail.com>
2026-05-28 13:36:11 +0300

    Added multiply function with tests

---
+++ src/my_package/__init__.py
@@ -0,0 +1,4 @@
+# src/my_package/__init__.py
+from .core import greet, add, multiply
+
+__version__ = "0.2.0"
--- src/my_package/core.py
+++ src/my_package/core.py
@@ -6,3 +6,7 @@ def greet(name: str) -> str:
 def add(a: int, b: int) -> int:
     """Return the sum of two integers."""
     return a + b
+
+def multiply(a: int, b: int) -> int:
+    """Return the product of two integers."""
+    return a * b
